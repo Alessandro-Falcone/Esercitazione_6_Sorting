28f4dff1185ca1afce1f4cb03c3352b5edc63dcd
alexf <alexf@LAPTOP-L8J25IH7>
2024-05-02 00:13:05 +0200

    main modificato

--- Esercizio_2/main.cpp
+++ Esercizio_2/main.cpp
@@ -15,17 +15,29 @@ int main(int argc, char **argv){
 
     if(argc != 2){
         cerr << "Errore: numero di argomenti passati da linea di comando sbagliato" << endl;
-        cout << "Oltre all'eseguibile che e' sempre il primo argomento passato da linea di comando deve essere passato un'altro argomento da linea di comando" << endl;
-        cout << "Primo argomento linea di comando: " << argv[0] << endl;
-        cout << "Secondo argomento linea di comando: <lunghezza vettore>" << endl;
+        cerr << "Oltre all'eseguibile che e' sempre il primo argomento passato da linea di comando deve essere passato un'altro argomento da linea di comando" << endl;
+        cerr << "Primo argomento linea di comando: " << argv[0] << endl;
+        cerr << "Secondo argomento linea di comando: <lunghezza vettore>" << endl;
         return 1;
     }else{
+
+        // ciclo for che controlla se il secondo argomento passato da linea di comando è un intero,
+        // andando a verificare fino al terminatore di stringa \0 se tutti i caratteri sono cifre
+        for(char *p = argv[1]; *p != '\0'; p++){
+            if(!isdigit(*p)){
+                // se almeno un carattere non è un cifra (condizione dell'if verifica che ogni carattere non è una cifra)
+                // il programma termina e oltre a restituire -1 stampa anche un messaggio d'errore
+                cerr << "Errore: il secondo argomento della linea di comando non e' un numero intero" << endl;
+                return 2;
+            }
+        }
+
         lunghezzaVettore = stoi(argv[1]);
     }
 
     if(lunghezzaVettore > 1e+3){
-        cerr << "Dimensione del vettore inserita da linea di comando troppo grande: " << lunghezzaVettore << endl;
-        return -1;
+        cerr << "Dimensione del vettore inserita da linea di comando troppo grande rischio crash del programma" << endl;
+        return 3;
     }
 
     vector<unsigned int> arr(lunghezzaVettore);
@@ -47,11 +59,17 @@ int main(int argc, char **argv){
         }
 
         // Misura del tempo per Bubble Sort
-        auto inizioBubble = high_resolution_clock::now(); // misura il tempo di inizio dell'algoritmo
+        auto inizioBubble = high_resolution_clock::now(); // misura il tempo di inizio dell'algoritmo        
         BubbleSort(arr); // chiamo l'algoritmo di Bubble Sort per il vettore // esegui Bubble Sort sull'array
         auto fineBubble = high_resolution_clock::now(); // misura il tempo di fine dell'algoritmo
-        auto durataBubble = duration_cast<microseconds>(fineBubble - inizioBubble); // calcolo la differenza tra il tempo di fine dell'algoritmo e il tempo di inizio dell'algoritmo usando come unità di misura i microsecondi
-        bubbleDur[n] = durataBubble.count(); // mi salvo nel vettore mergeDur[n] quanto tempo ci ha messo l'algoritmo Bubble Sort per ordinare il vettore
+
+        // calcolo la differenza tra il tempo di fine dell'algoritmo
+        // e il tempo di inizio dell'algoritmo usando come unità di misura i microsecondi
+        auto durataBubble = duration_cast<microseconds>(fineBubble - inizioBubble);
+
+        // mi salvo nel vettore bubbleDur[n] quanto tempo ci ha messo l'algoritmo Bubble Sort
+        // per ordinare il vettore ad ogni iterazione (numeroIterazioni)
+        bubbleDur[n] = durataBubble.count();
     }
 
     // Calcolo della media delle durate per Bubble Sort
@@ -60,6 +78,7 @@ int main(int argc, char **argv){
     for(unsigned int i = 0; i < numeroIterazioni; i++) {
         bubbleSommaDurata += bubbleDur[i];
     }
+
     bubbleDurataMedia = (bubbleSommaDurata*1e-3)/numeroIterazioni; // ho moltiplicato per 1e-3 in modo da non avere numeri troppo grandi espressi in microsecondi
 
     vector<double> mergeDur(numeroIterazioni);
@@ -75,7 +94,7 @@ int main(int argc, char **argv){
         MergeSort(arr); // esegui Merge Sort sull'array
         auto fineMerge = high_resolution_clock::now(); // misura il tempo di fine dell'algoritmo
         auto durataMerge = duration_cast<microseconds>(fineMerge - inizioMerge); // calcolo la differenza tra il tempo di fine dell'algoritmo e il tempo di inizio dell'algoritmo usando come unità di misura i microsecondi
-        mergeDur[n] = durataMerge.count(); // mi salvo nel vettore mergeDur[n] quanto tempo ci ha messo l'algoritmo Merge Sort per ordinare il vettore
+        mergeDur[n] = durataMerge.count(); // mi salvo nel vettore mergeDur[n] quanto tempo ci ha messo l'algoritmo Merge Sort per ordinare il vettore ad ogni iterazione (numeroIterazioni)
     }
 
     // Calcolo della media delle durate per Merge Sort
@@ -103,31 +122,28 @@ int main(int argc, char **argv){
         cout << "Media tempo Merge Sort su " << numeroIterazioni << " esecuzioni: " << fixed << scientific << setprecision(7) << mergeDurataMedia << " microsecondi" << endl;
     }
 
+    if(bubbleDurataMedia == 0 || mergeDurataMedia == 0){
+        cout << "Errore: tempo in microsecondi pari a zero, dimensione del vettore inserita da linea di comando molto piccola cambiare dimensione"
+                " o calcolare il tempo con un'unita' di misura piu' piccola dei microsecondi" << endl;
+    }
+
     if(bubbleDurataMedia < mergeDurataMedia){
-
-        cout << "L'algoritmo Bubble Sort e' piu' veloce dell'algoritmo Merge Sort per la lunghezza inserita (lunghezza vettore: "
-             << lunghezzaVettore << ")" << endl;
-
+        cout << "L'algoritmo Bubble Sort e' piu' veloce dell'algoritmo Merge Sort per la lunghezza inserita" << endl;
     }else if(bubbleDurataMedia > mergeDurataMedia){
-
-        cout << "L'algoritmo Bubble Sort e' piu' lento dell'algoritmo Merge Sort per la lunghezza inserita (lunghezza vettore: "
-             << lunghezzaVettore << ")" << endl;
-
+        cout << "L'algoritmo Bubble Sort e' piu' lento dell'algoritmo Merge Sort per la lunghezza inserita" << endl;
     }else if(bubbleDurataMedia == mergeDurataMedia){
-
-        cout << "Gli algoritmi Bubble Sort e Merge Sort hanno la stessa durata per la lunghezza inserita (lunghezza vettore: "
-             << lunghezzaVettore << ")" << endl;
+        cout << "Gli algoritmi Bubble Sort e Merge Sort hanno la stessa durata per la lunghezza inserita" << endl;
     }
 
     return 0;
 }
 
 // Bubble Sort: algoritmo di ordinamento semplice ma inefficiente per grandi insiemi di dati.
-// La sua complessità computazionale è di O(n^2), dove n è la dimensione dell'array da ordinare
+// La sua complessità computazionale è di O(n^2) dove n è la dimensione dell'array da ordinare
 // questo significa che il tempo di esecuzione cresce quadraticamente con la dimensione dell'array
 // ma questo lo rende poco efficiente per grandi quantità di dati.
 
 // Merge Sort: Merge Sort è un algoritmo di ordinamento più efficiente del Bubble Sort.
-// La sua complessità computazionale è di O(n*log(n)), dove n è la dimensione dell'array da ordinare
+// La sua complessità computazionale è di O(n*log(n)) dove n è la dimensione dell'array da ordinare
 // questo lo rende significativamente più veloce rispetto al Bubble Sort,
 // specialmente quando si tratta di grandi volumi di dati.
